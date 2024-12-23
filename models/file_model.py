20733f667c41fa5496dc5ec86e192f9fc5a9e491
Nabemh <nhanior@gmail.com>
2024-12-23 06:30:17 +0100

    database models completed

---
+++ models/file_model.py
@@ -0,0 +1,12 @@
+files_collection = db['files']
+
+def save_metadata(username, file_name, file_path, s3_url):
+    files_collection.insert_one({
+        'username': username,
+        'file_name': file_name,
+        'file_path': file_path,
+        's3_url': s3_url
+    })
+
+def get_metadate(username):
+    return list(files_collection.find({'username' : username}))
--- models/user_model.py
+++ models/user_model.py
@@ -9,4 +9,19 @@ def register_user(username, password):
     if users.find_one({'username': username}):
         return False
     hashed_password = generate_password_hash(password)
-    users.insert_one({'username': username, })
+    users.insert_one({'username': username, 'password': hashed_password})
+
+    return True
+
+def authenticate_user(username, password):
+    user = users.find_one({'username' : username})
+
+    if user is None:
+        return False
+    
+    stored_hash = user['password']
+
+    if check_password_hash(stored_hash, password):
+        return True
+    else:
+        return False
